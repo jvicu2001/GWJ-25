b4f6e6dcb1fe2569700abedc19775f077fed1c2e
Jorge Vicuña <jvicuna2001@gmail.com>
2020-09-12 13:21:48 -0300

    Initial progress
    Added a test terrain, ball movement and debug resizing

---
+++ Assets/Scenes/Mechanics_test.gd
@@ -0,0 +1,21 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var velocity = Vector3()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = $Ball/BallBody.get_linear_velocity()
+	$Label.text = 	"x: " + str(velocity.x) +\
+					"\ny: " + str(velocity.y) +\
+					"\nz: " + str(velocity.z)
+	pass
